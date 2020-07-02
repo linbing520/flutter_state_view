70db45bb6aead93cb854fa826423091e3f0bd0db
linbing <linbing@kaopuyun.com>
2020-07-02 10:35:54 +0800

    版本修改

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_pager_list/main.dart';
+import 'package:flutter_state_view/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
